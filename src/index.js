42b1388a9041b7f72668128087d50d12bd5edbcd
Afrin23 <aafrinbanu.j@gmail.com>
2024-09-14 18:51:29 +0530

    files uploaded

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./style.css";
+import img1 from "../src/assets/images/bird.jpeg";
+import img2 from "../src/assets/images/dove.jpeg";
+import img3 from "../src/assets/images/crane.jpeg";
+import img4 from "../src/assets/images/eagle.jpeg";
+import img5 from "../src/assets/images/peco.jpeg";
+import img6 from "../src/assets/images/flam.jpeg";
+import img7 from "../src/assets/images/kf.jpeg";
+import img8 from "../src/assets/images/os.jpeg";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Gallery(props) {
+  return (
+    <div className='container img'>
+      <div className='bind'>
+      <img src={props.image} alt="bird"></img> 
+      <h2 className="name">{props.name}</h2>
+      </div>
+    </div>
+  )
+}
+
+const photo = [
+  {
+    image: img6,
+    name: "Flamingo"
+  },
+  {
+    image: img2,
+    name: "Dove"
+  },
+  {
+    image: img7,
+    name: "KingFisher"
+  },
+  {
+    image: img4,
+    name: "Eagle"
+  },
+  {
+    image: img5,
+    name: "Peacock"
+  },
+  {
+    image: img1,
+    name: "Linet"
+  },
+  {
+    image: img3,
+    name: "Crane"
+  },
+  {
+    image: img8,
+    name: "Ostrich"
+  }
+];
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className='container'>
+    {photo.map((item, index) => (
+      <Gallery key={index} image={item.image} name={item.name} />
+    ))}
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
